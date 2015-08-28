c309764cd5e5c966d497df44001fadcd74e68c8b
Paul Tagliamonte <paultag@gmail.com>
2015-08-27 23:14:17 -0400

    add rpc hookup

--- remote.go
+++ remote.go
@@ -1,8 +1,27 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"log"
+
+	"pault.ag/go/service"
+)
 
 func BeARemote(config MinionConfig) {
+	node := MinionNode{}
+	log.Printf("Bringing remote online\n")
+	node.Register()
+	log.Printf("Diling coordinator\n")
+	conn, err := service.DialFromKeys(
+		fmt.Sprintf("%s:%d", config.Host, config.Port),
+		config.Cert, config.Key, config.CaCert,
+	)
+	if err != nil {
+		log.Fatalf("Error! %s\n", err)
+	}
+	log.Printf("Bringing RPC online")
+	client := service.Client(conn)
+	log.Printf("%s\n", client)
 }
 
 // vim: foldmethod=marker
